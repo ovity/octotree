50fd3a3ad3eeb6093d47871cd3c42d9e6b714a00
Buu Nguyen <buunguyen@gmail.com>
2014-05-09 17:06:38 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-octotree
-========
+## Octotree
+Chrome extension to display GitHub code in tree format. Useful for code junkies like me who frequently read source in GitHub and do not want download or checkout every single repository.
+
+## Install
+* Download and install [Octotree](tbd) from the Chrome store
+* Navigate to any GitHub project (or just refresh this page as an example)
+* The code tree should show as follows:
+
+![When extension is active](https://github.com/buunguyen/octotree/screen_ext.png)
+
+## GitHub API Rate Limit
+Octotree uses [GitHub API](https://developer.github.com/v3/) to retrieve repository metadata. By default, it makes unauthenticated requests to the GitHub API. However, there are two situations when requests must be authenticated:
+
+* You are accessing a private repository
+* You exceed the [rate limit of unauthenticated requests](https://developer.github.com/v3/#rate-limiting)
+
+When that happens, Octotree will show use the following screen to ask for your [GitHub personal access token](https://help.github.com/articles/creating-an-access-token-for-command-line-use). 
+
+![Enter personal access token](https://github.com/buunguyen/octotree/screen_token.png)
+
+If you don't already have one, create one at [this page](https://github.com/settings/tokens/new). Then enter the generated token into the textbox and save.
+
+Alternatively, you can always manually enter or update the token by following these steps:
+
+* Navigate to any GitHub page
+* Open the Chrome developer console
+* Execute the following line:
+```javascript
+localStorage.setItem('octotree.github_access_token', 'REPLACE WITH TOKEN')
+```
+
+## Contribution
+There are several improvements that can be made to Octotree. Contribution is very welcome.
+
+- [ ] Make the width of the tree panel resizable
+- [ ] Allow users to enter access token any time (i.e. a new button?)
+- [ ] Synchronize selection if users click on GitHub links directly (instead of via Octotree)
+- [ ] Show progress indicator while a repository is being loaded
+
+## Credit
+[Icon](https://github.com/pstadler/octofolders) by [pstadler](https://github.com/pstadler)
---
+++ inject.js
@@ -0,0 +1,196 @@
+(function() {
+  const PREFIX = 'octotree'
+      , TOKEN  = 'octotree.github_access_token'
+      , SHOWN  = 'octotree.shown'
+      // ugly, I know, can it be improved?
+      , REGEXP = /([^\/]+)\/([^\/]+)(?:\/([^\/]+))?/
+      
+  var $html    = $('html')
+    , $sidebar = $('<nav class="octotree_sidebar">' +
+                     '<h1>loading...</h1>' +
+                     '<div class="tree"></div>' +
+                   '</nav>')
+    , $tree    = $sidebar.find('.tree')
+    , $token   = $('<form>' +
+                     '<div>' +
+                       '<input name="token" type="text" placeholder="Enter personal access token"></input>' +
+                     '</div>' +
+                     '<div>' +
+                       '<button type="submit">Save</button>' +
+                       '<a href="https://github.com/settings/tokens/new" target="_blank">Create token</a> | ' +
+                       '<a href="https://github.com/buunguyen/octotree" target="_blank">Help</a>' +
+                     '</div>' +
+                     '<div class="error"></div>' +
+                   '</form>')
+    , $toggler = $('<div class="octotree_toggle">&#9776;</div>')
+    , store    = new Storage()
+    , selectedItem
+
+  $(document).ready(function() {
+    loadRepo(true)
+  })
+
+  function loadRepo(initDom) {
+    var repo = getRepoFromPath()
+    if (repo) {
+      if (initDom) {
+        $('body')
+          .append($sidebar)
+          .append($toggler.click(toggleSidebar))
+      }
+      fetchData(repo, function(err, tree) {
+        if (err) return onFetchError(err)
+        renderTree(repo, tree)
+      })
+    }
+  }
+
+  function getRepoFromPath() {
+    var match = location.pathname.match(REGEXP)
+    if (!match) return false
+
+    // must not be a reserved `username`
+    if (~['settings', 'organizations', 'site'].indexOf(match[1])) return false
+
+    // if match[3] exists, it must be either 'tree' or 'blob'
+    // TODO: problem is, when navigate back, say from Issues to code, need to detect & refetch
+    // if (match[3] && !~['tree', 'blob'].indexOf(match[3])) return false
+
+    return { 
+      username : match[1], 
+      reponame : match[2],
+      branch   : $('*[data-master-branch]').data('ref') || 'master'
+    }
+  }
+
+  function fetchData(repo, done) {
+    var github  = new Github({ token: store.get(TOKEN) })
+      , api     = github.getRepo(repo.username, repo.reponame)
+      , root    = []
+      , folders = { '': root }
+
+    api.getTree(repo.branch + '?recursive=true', function(err, tree) {
+      if (err) return done(err)
+      tree.forEach(function(item) {
+        var path   = item.path
+          , index  = path.lastIndexOf('/')
+          , name   = path.substring(index + 1)
+          , folder = folders[path.substring(0, index)]
+
+        folder.push(item)
+        item.text = name
+        if (item.type === 'tree') {
+          folders[item.path] = item.children = []
+          item.icon = 'dir'
+        } else {
+          // there should be a better way than this...
+          item._url = '/' + repo.username + '/' + repo.reponame + '/blob/' + repo.branch + '/' + path
+          item.icon = 'file'
+        }
+      })
+
+      done(null, sort(root))
+
+      function sort(folder) {
+        folder.sort(function(a, b) {
+          if (a.type === b.type) return a.text.localeCompare(b.text)
+          return a.type === 'tree' ? -1 : 1
+        })
+        folder.forEach(function(item) {
+          if (item.type === 'tree') sort(item.children)
+        })
+        return folder
+      }
+    })
+  }
+
+  function onFetchError(err) {
+    var msg = 'Error: ' + err.error
+    if (err.error === 401)
+      msg = 'Invalid token!'
+    else if (err.error === 404) 
+      msg = 'Private or invalid repository!'
+    else if (err.error === 403 && ~err.request.getAllResponseHeaders().indexOf('X-RateLimit-Remaining: 0')) 
+      msg = 'API limit exceeded!'
+    updateSidebar(msg, true)
+  }
+
+  function renderTree(repo, tree) {
+    $tree
+      .empty()
+      .jstree({
+        core    : { data: tree, animation: 100 },
+        plugins : ['wholerow', 'state'],
+        state   : { key : PREFIX + '.' + repo.username + '/' + repo.reponame }
+      })
+      .delegate('.jstree-open>a', 'click.jstree', function() {
+        $.jstree.reference(this).close_node(this)
+      })
+      .delegate('.jstree-closed>a', 'click.jstree', function() {
+        $.jstree.reference(this).open_node(this)
+      })
+
+      // The next 2 listeners are ugly
+      // basically I need a way to distingush between selection caused by user clicking a node 
+      // and selection caused during restoration phase of jsTree's state plugin
+      // The former happens to trigger a `click` after `select_node`
+      .bind('select_node.jstree', function(e, data) {
+        selectedItem = data.node.original
+      })
+      .bind('click.jstree', function() {
+        if (selectedItem && selectedItem._url) window.location.href = selectedItem._url
+      })
+      .bind('ready.jstree', function() {
+        updateSidebar(repo.username + ' / ' + repo.reponame + ' [' + repo.branch + ']')  
+      })
+  }
+
+  function updateSidebar(header, askForToken) {
+    $sidebar.find('h1').text(header)
+
+    if (askForToken) {
+      $tree.empty().append($token.submit(saveToken))
+    }
+
+    // Shows sidebar when:
+    // 1. ask for access token
+    // 2. first time extension is used
+    // 3. if it was previously shown
+    if (askForToken || store.get(SHOWN) !== false) {
+      $html.addClass(PREFIX)
+      store.set(SHOWN, true)
+    }
+  }
+
+  function toggleSidebar() {
+    var shown = store.get(SHOWN)
+    if (shown) $html.removeClass(PREFIX)
+    else $html.addClass(PREFIX)
+    store.set(SHOWN, !shown)
+  } 
+
+  function saveToken(event) {
+    event.preventDefault()
+
+    var token = $token.find('[name="token"]').val()
+      , $error = $token.find('div.error').text('')
+
+    if (token === '') {
+      return $error.text('Token is required')
+    }
+    store.set(TOKEN, token)
+    loadRepo()
+  }   
+
+  function Storage() {
+    this.get = function(key) {
+      return JSON.parse(localStorage.getItem(key))
+    }
+    this.set = function(key, val) {
+      return localStorage.setItem(key, JSON.stringify(val))
+    }
+    this.del = function(key) {
+      return localStorage.removeItem(key)
+    }
+  }
+})()
