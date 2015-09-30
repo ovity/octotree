facb56f8c154154b645adbf3d5aad6025b779abe
Buu Nguyen <buunguyen@gmail.com>
2015-09-30 16:25:13 -0700

    Fixes #189 and releases

--- src/util.location.js
+++ src/util.location.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
   // When navigating from non-code pages (i.e. Pulls, Issues) to code page
   // GitHub doesn't reload the page but uses pjax. Need to detect and load Octotree.
-  var href, hash
+  var firstLoad = true, href, hash
   function detectLocationChange() {
     if (location.href !== href || location.hash !== hash) {
       href = location.href
       hash = location.hash
-      $(document).trigger(EVENT.LOC_CHANGE, href, hash)
+
+      // If this is the first time this is called, no need to notify change as
+      // Octotree does its own initialization after loading options.
+      if (firstLoad) {
+        firstLoad = false
+      }
+      else {
+        setTimeout(function () {
+          $(document).trigger(EVENT.LOC_CHANGE, href, hash)
+        }, 200) // Waits a bit for pjax DOM change
+      }
     }
+
     setTimeout(detectLocationChange, 200)
   }
   detectLocationChange()
