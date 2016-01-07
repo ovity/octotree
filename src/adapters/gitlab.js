f2812bf0733f33176e614e1e0b4f80d4f2deaeb8
Philipp Meier <phimei@gmx.ch>
2016-01-07 08:08:42 +0100

    Fix GitLab fallback for project/repo URLs with projectId instead

--- src/adapters/gitlab.js
+++ src/adapters/gitlab.js
@@ -8,6 +8,7 @@ const GL_SIDEBAR = '.sidebar-wrapper'
 const GL_TITLE = 'h1.title'
 const GL_CONTENT = '.container>.content'
 const GL_MAIN_NAV = '.main-nav'
+const GL_PROJECT_ID = '#project_id'
 
 class GitLab extends Adapter {
 
@@ -199,7 +200,12 @@ class GitLab extends Adapter {
   _get(path, opts, cb) {
     const repo = opts.repo
     const host = `${location.protocol}//${location.host}/api/v3`
-    const url = `${host}/projects/${repo.username}%2f${repo.reponame}/repository${path}&private_token=${opts.token}`
+    // GitLab has some issues with Apache and Nginx configuration for proj/repo
+    // URLs. Thus if project_id is present use it instead of proj/rep.
+    // More see here: https://github.com/gitlabhq/gitlabhq/issues/8290#issuecomment-127040461
+    const url = ($(GL_PROJECT_ID).length) ?
+      `${host}/projects/${$(GL_PROJECT_ID).val()}/repository${path}&private_token=${opts.token}` :
+      `${host}/projects/${repo.username}%2f${repo.reponame}/repository${path}&private_token=${opts.token}`
     const cfg = { url, method: 'GET', cache: false }
 
     $.ajax(cfg)
