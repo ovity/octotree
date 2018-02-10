dea290ef6e1d1fd94b138efb024e578e5c91601d
Giacomo Rossetto <Jackymancs4@users.noreply.github.com>
2018-02-10 18:42:07 +0100

    Set default options when creating GitHub token

--- src/adapters/github.js
+++ src/adapters/github.js
@@ -58,7 +58,7 @@ class GitHub extends PjaxAdapter {
 
   // @override
   getCreateTokenUrl() {
-    return `${location.protocol}//${location.host}/settings/tokens/new`
+    return `${location.protocol}//${location.host}/settings/tokens/new?scopes=repo&description=Octotree%20browser%20extension`
   }
 
   // @override
