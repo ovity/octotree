f83b70ce522d42c722bd44ea7e2c58fa99a9f557
Steven Noto <snoto@simplyautomatic.com>
2018-10-16 20:18:54 -0500

    Consolidate page detection logic.

--- src/adapters/github.js
+++ src/adapters/github.js
@@ -118,17 +118,15 @@ class GitHub extends PjaxAdapter {
     const pullNumber = isPR && showOnlyChangedInPR ? typeId : null
 
     // Skip non-code page unless showInNonCodePage is true
-    const isCodePage = ['tree', 'blob', 'commit'].indexOf(type) >= 0
-    if (!showInNonCodePage && type && !isCodePage) {
+    const isCodePage = !type || ['tree', 'blob', 'commit'].indexOf(type) >= 0
+    if (!showInNonCodePage && !isCodePage) {
       return cb()
     }
 
     // Get branch by inspecting page, quite fragile so provide multiple fallbacks
-    const isBranchOrSha = isCodePage && typeId
-    const branchOrSha = isBranchOrSha && typeId ? typeId : null
     const branch =
-      // Branch/Tag/Commit from URL
-      branchOrSha ||
+      // Branch/tag/commit from URL
+      (isCodePage && typeId) ||
       // Code page
       $('.branch-select-menu .select-menu-item.selected').data('name') ||
       // Pull requests page
