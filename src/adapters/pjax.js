601553576d9f8dcaa87cff8ba81b10901ff1db6d
Joseph Frazier <1212jtraceur@gmail.com>
2019-01-26 19:03:17 -0500

    Dispatch 'pjax:end' event on native document
    This attempts to address https://github.com/ovity/octotree/issues/490,
    but I haven't yet tested it myself.

--- src/adapters/pjax.js
+++ src/adapters/pjax.js
@@ -4,7 +4,10 @@ class PjaxAdapter extends Adapter {
 
     $(document)
       .on('pjax:start', () => $(document).trigger(EVENT.REQ_START))
-      .on('pjax:end', () => $(document).trigger(EVENT.REQ_END))
+      .on('pjax:end', () => {
+        document.dispatchEvent(new Event(EVENT.REQ_END));
+        return $(document).trigger(EVENT.REQ_END);
+      })
       .on('pjax:timeout', (e) => e.preventDefault());
   }
 
