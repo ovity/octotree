22d621593f51c62b1ab8145bab2809cfbe68ebdb
NDQ <endyquang@gmail.com>
2019-09-03 14:32:43 +0700

    PR page smooth scroll

--- src/adapters/adapter.js
+++ src/adapters/adapter.js
@@ -266,6 +266,19 @@ class Adapter {
    * @api public
    */
   selectFile(path) {
+
+    // Smooth scroll to diff file on PR page
+    const diffMatch = path.match(/#diff-\d+$/);
+    if (diffMatch) {
+      const el = $(diffMatch[0])
+      if (el) {
+        $('html, body').animate({
+          scrollTop: el.offset().top - 68
+        }, 600);
+        return
+      }
+    }
+
     window.location.href = path;
   }
 
