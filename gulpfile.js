c46ade63b0d3bf7243795f8a4443225724d1fdef
Buu Nguyen <buunguyen@gmail.com>
2019-11-21 11:43:06 -0800

    Remove footer view

--- gulpfile.js
+++ gulpfile.js
@@ -173,7 +173,6 @@ function buildJs(prefix = '.', ctx = {}) {
     `${prefix}/src/view.error.js`,
     `${prefix}/src/view.tree.js`,
     `${prefix}/src/view.options.js`,
-    `${prefix}/src/view.footer.js`,
     `${prefix}/src/main.js`
   ];
 
