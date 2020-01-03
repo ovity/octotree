6a362bcc4be41b6f2f93093ce17291b896d04527
Buu Nguyen <buunguyen@gmail.com>
2020-01-03 01:21:48 -0800

    Revert "Fix layout on New PR page" (#870)
    This reverts commit d6200d8d0ac7ec2a96a51073360736dc5190c425.

--- src/adapters/github.js
+++ src/adapters/github.js
@@ -7,6 +7,7 @@ const GH_PJAX_CONTAINER_SEL =
   '#js-repo-pjax-container, div[itemtype="http://schema.org/SoftwareSourceCode"] main, [data-pjax-container]';
 
 const GH_CONTAINERS = '.container, .container-lg, .container-responsive';
+const GH_HEADER = '.js-header-wrapper > header';
 const GH_MAX_HUGE_REPOS_SIZE = 50;
 const GH_HIDDEN_RESPONSIVE_CLASS = '.d-none';
 const GH_RESPONSIVE_BREAKPOINT = 1010;
@@ -77,6 +78,7 @@ class GitHub extends PjaxAdapter {
   // @override
   updateLayout(sidebarPinned, sidebarVisible, sidebarWidth) {
     const SPACING = 10;
+    const $header = $(GH_HEADER);
     const $containers =
       $('html').width() <= GH_RESPONSIVE_BREAKPOINT
         ? $(GH_CONTAINERS).not(GH_HIDDEN_RESPONSIVE_CLASS)
@@ -84,9 +86,17 @@ class GitHub extends PjaxAdapter {
 
     const autoMarginLeft = ($(document).width() - $containers.width()) / 2;
     const shouldPushEverything = sidebarPinned && sidebarVisible;
+    const smallScreen = autoMarginLeft <= sidebarWidth + SPACING;
 
-    $('html').css('margin-left', shouldPushEverything ? sidebarWidth : '');
-    $containers.css('margin-left', shouldPushEverything ? Math.max(SPACING, autoMarginLeft - sidebarWidth) : '');
+    $('html').css('margin-left', shouldPushEverything && smallScreen ? sidebarWidth : '');
+    $containers.css('margin-left', shouldPushEverything && smallScreen ? SPACING : '');
+
+    if (shouldPushEverything && !smallScreen) {
+      // Override important in Github Header class in large screen
+      $header.attr('style', `padding-left: ${sidebarWidth + SPACING}px !important`);
+    } else {
+      $header.removeAttr('style');
+    }
   }
 
   // @override
