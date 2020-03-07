4abc360e6aa243763d09fc7d71e4c06d18a98856
Danh Nguyen <danhnguyen30041975@gmail.com>
2020-03-07 18:01:15 +0700

    Relax checking diffId

--- src/adapters/adapter.js
+++ src/adapters/adapter.js
@@ -89,7 +89,7 @@ class Adapter {
               }
 
               // If item is part of a PR, jump to that file's diff
-              if (item.patch && typeof item.patch.diffId === 'number') {
+              if (item.patch && item.patch.diffId) {
                 const url = this._getPatchHref(repo, item.patch);
                 item.a_attr = {
                   href: url,
